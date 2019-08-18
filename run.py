9b580b1a3e943ebb728ff1b6a0f4115111dbc75a
jsafranek <safranek.h@gmail.com>
2019-08-18 15:35:43 +0200

    Add function to post to user's fb wall

--- run.py
+++ run.py
@@ -21,4 +21,6 @@ if access_token is None:
 # Facebook SDK for Python
 graph = facebook.GraphAPI(access_token=access_token, version="2.12")
 
-print("hello, world! <3")
+print("\n## put_object():\n")
+# Write 'Hello, world' to the active user's wall.
+graph.put_object(parent_object='me', connection_name='feed', message='face-the-forest')
