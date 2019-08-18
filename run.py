59c4d64afa42019599ccead65eeb72d30f6275ee
jsafranek <safranek.h@gmail.com>
2019-08-18 11:34:42 +0200

    Read access_token from config file

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+import configparser
+
 import facebook
+
+# Read from config file
+config = configparser.ConfigParser()
+config.read('etc/facetheforest.conf')
+
+# Get credentials from config file
+access_token = config['facebook']['access_token']
+
+# Facebook SDK for Python
+graph = facebook.GraphAPI(access_token=access_token, version="2.12")
+
 
 print("hello, world! <3")
