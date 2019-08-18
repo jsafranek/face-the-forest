d29cb0c76bd66e79c7984c2aa10663e0e2d44566
jsafranek <safranek.h@gmail.com>
2019-08-18 13:22:55 +0200

    Edit config comment

--- run.py
+++ run.py
@@ -7,11 +7,10 @@ import facebook
 config = configparser.ConfigParser()
 config.read('etc/facetheforest.conf')
 
-# Get credentials from config file
+# Get credentials from config
 access_token = config['facebook']['access_token']
 
 # Facebook SDK for Python
 graph = facebook.GraphAPI(access_token=access_token, version="2.12")
-
 
 print("hello, world! <3")
